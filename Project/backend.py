f123b54a3234b70980ca6b18686fd6e9ded1c0bc
Yehang10 <yehangrai10@gmail.com>
2021-05-30 20:09:07 +0545

    initial commit

---
+++ Project/backend.py
@@ -0,0 +1,8 @@
+def Database():
+    global conn, cursor
+    #creating student database
+    conn = sqlite3.connect("student.db")
+    cursor = conn.cursor()
+    #creating STUD_REGISTRATION table
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS STUD_REGISTRATION (STU_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, STU_NAME TEXT, STU_CONTACT TEXT, STU_EMAIL TEXT, STU_ROLLNO TEXT, STU_BRANCH TEXT)")
