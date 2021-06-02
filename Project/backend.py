0394f473ef533edfa6a4c648ec9f9c6a35417a37
Yehang10 <yehangrai10@gmail.com>
2021-06-02 19:48:16 +0545

    delete and search function added

--- Project/backend.py
+++ Project/backend.py
@@ -3,9 +3,9 @@ def Database():
     #creating student database
     conn = sqlite3.connect("student.db")
     cursor = conn.cursor()
-    #creating STUD_REGISTRATION table
+    #creating STUD_DATAA table
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS STUD_DATAAA(STU_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, STU_NAME TEXT, STU_CONTACT TEXT, STU_EMAIL TEXT, STU_ROLLNO TEXT, STU_BRANCH TEXT)")
+        "CREATE TABLE IF NOT EXISTS STU_DATAAA(STU_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, STU_NAME TEXT, STU_CONTACT TEXT, STU_EMAIL TEXT, STU_ROLLNO TEXT, STU_BRANCH TEXT)")
 
 
 def register():
@@ -48,3 +48,41 @@ def Reset():
     email.set("")
     rollno.set("")
     branch.set("")
+
+
+def delete():
+    # open database
+    Database()
+    if not tree.selection():
+        tkMessageBox.showwarning("Warning", "Select data to delete")
+    else:
+        result = tkMessageBox.askquestion('Confirm', 'Are you sure you want to delete this record?',
+                                          icon="warning")
+        if result == 'yes':
+            curItem = tree.focus()
+            contents = (tree.item(curItem))
+            selecteditem = contents['values']
+            tree.delete(curItem)
+            cursor = conn.execute("DELETE FROM STU_DATAAA WHERE STU_ID = %d" % selecteditem[0])
+            conn.commit()
+            cursor.close()
+            conn.close()
+def SearchRecord():
+    #open database
+    Database()
+    #checking search text is empty or not
+    if SEARCH.get() != "":
+        #clearing current display data
+        tree.delete(*tree.get_children())
+        #select query with where clause
+        cursor=conn.execute("SELECT * FROM STU_DATAAA WHERE STU_NAME LIKE ?", ('%' + str(SEARCH.get()) + '%',))
+        #fetch all matching records
+        fetch = cursor.fetchall()
+        #loop for displaying all records into GUI
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()
+
+
+
